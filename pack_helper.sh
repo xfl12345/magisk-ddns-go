5b9a0c5d2dd81f799caf77362280546e1e5cce05
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2024-04-28 18:55:42 +0800

    尝试提高压缩率

--- pack_helper.sh
+++ pack_helper.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-zip magisk-ddns-go.zip ./ -r -x LICENSE -x README.MD -x '.git/*' -x magisk-ddns-go.zip -x pack_helper.sh -x '.gitignore' -x 'bin/.gitignore' -x 'config/.gitignore'
+zip magisk-ddns-go.zip ./ -9 -r -x LICENSE -x README.MD -x '.git/*' -x magisk-ddns-go.zip -x pack_helper.sh -x '.gitignore' -x 'bin/.gitignore' -x 'config/.gitignore'
